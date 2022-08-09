75e47728340909cfff349d8e0400c8e9db3d0e18
PrestonS95 <prestonsullins95@gmail.com>
2022-08-09 12:58:00 -0500

    Pushing file structure

---
+++ build/precache-manifest.0307d63e8cf0b4706efc7e9f17f67bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30260019fa7b2d31890786484a9aa97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82a90f89172a8157da3b",
+    "url": "/static/css/main.9b8c1b41.chunk.css"
+  },
+  {
+    "revision": "5e0b0e50504c55111401",
+    "url": "/static/js/2.ab0155c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab0155c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a90f89172a8157da3b",
+    "url": "/static/js/main.60c83118.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
