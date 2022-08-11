15bba150846206d79d161a5358e4e59123500daf
Jessica <kimjessicad@gmail.com>
2022-08-11 13:32:35 -0500

    router changes

--- api/products.js
+++ api/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { requireUser } = require("./utils");
 const { getAllProducts, createProducts, getProductByCategory, getProductById, updateProduct, deleteProduct } = require("../db/models")
 
-router.get('/', async (req, res, next) => {
+apiRouter.get('/', async (req, res, next) => {
     try {
         const allProducts = await getAllProducts();
         res.send(allProducts);
@@ -12,9 +12,9 @@ router.get('/', async (req, res, next) => {
     }
 })
 
-router.post('/', requireAdminUser, async (req, res, next) => {
+apiRouter.post('/', requireAdminUser, async (req, res, next) => {
     const { title, brand, description, price, inventory, category, image } = req.body;
-    const _product = await createProduct({ title, brand, description, price, inventory, category, image })
+    const _product = await createProducts({ title, brand, description, price, inventory, category, image })
     try {
         if (_product) {
             res.send(_product)
@@ -23,3 +23,25 @@ router.post('/', requireAdminUser, async (req, res, next) => {
         next(error);
     }
 })
+
+// apiRouter.patch('/:productId', requireAdminUser, async (req, res, next) => {
+//     const { title, brand, description, price, inventory, category, image } = req.body;
+//     const id = req.params.routineId;
+//     const routine = await getProductsById(id)
+//     try {
+//         if (!user.isAdmin) {
+//             res.status(403)
+//             next({
+//                 name: "UserNotAdminUserOrFound",
+//                 message: `User ${req.user.username} is not allowed to update ${routine.name}`,
+//             });
+//         }
+//         const updatedProduct = await updateProduct({ title, brand, description, price, inventory, category, image });
+//         res.send(updatedProduct);
+//     } catch ({ name, message }) {
+//         next({ name, message });
+//     }
+// })
+
+
+module.exports = apiRouter;
