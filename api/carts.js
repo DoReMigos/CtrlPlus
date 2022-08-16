98d6ef9f5074ba07f9f732fda31ab7ac44e3bfc6
Simu <kangtaeam@gmail.com>
2022-08-16 13:59:08 -0400

    apirouter for carts

--- api/carts.js
+++ api/carts.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const apiRouter = express.Router();
 const { requireUser } = require("./utils");
-const [User,Cart,Order] = require("../db/models");
+const [User, Cart, Order] = require("../db/models");
 
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
+
+//getAllCarts across website hehe
+apiRouter.get("/", async (req, res, next ) => {
+    try {
+        const getAllCarts = await Cart.getAllCarts()
+        console.log(getAllCarts)
+        res.send(getAllCarts)
+    } catch (error) {
+       next(error) 
+    }
+})
+
+//grabbing current cart
+apiRouter.get("/cart", requireUser, async (req, res, next) => {
+    const {id} = req.user
+    try {
+        const Cart = await Cart.getCartById ({ id })
+        res.send(Cart);
+    } catch (error) {
+        next(error)
+    }
+}
+)
+apiRouter.get("./cart")
