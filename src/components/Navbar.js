fa1a298903f9f6829b02103cf1f2f8445fe07059
Ruby Gonzalez <RubyJ.Gonzalez@outlook.com>
2022-08-16 21:47:12 -0500

    Profile Page Error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,Navigate,useNavigate } from "react-router-dom";
 let Logo = require("./CtrlPlusLogo.png")
 
 export default function Navbar() {
+  let navigate = useNavigate()
+  function handleChange(value){
+    navigate(`${value}`)
+    value = ""
+  }
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container-fluid">
           <Link to = "/">
-            <img src = {Logo} style = {{width: "60px", height: "60px"}}/></Link><h2 className = "text-info bg-dark">CTRL+</h2>
+            <img src = {Logo} style = {{width: "60px", height: "60px"}}/></Link><h2 className = "bg-dark" style = {{color: "#60dde2"}}>CTRL+</h2>
           {/* <h4 className="text-info bg-dark">CTRL +</h4> */}
           <button
             className="navbar-toggler"
@@ -30,14 +35,20 @@ export default function Navbar() {
                     to="/Store"
                     className="nav-link active"
                   >
-                    <p className="text-info bg-dark"> Store</p>
+                    <p className="bg-dark" style = {{color: "#60dde2"}}> Store</p>
                   </Link>
                 </div>
               </li>
               <li className="nav-item">
                 <div>
                   <Link to="/Store" className="nav-link">
-                  <p className="text-info bg-dark"> Support</p>
+                  {/* <p className="text-info bg-dark"> Info Tab</p> */}
+                  <select onChange  ={event => handleChange(event.target.value)}style = {{background:"#222529", color:"white", border: "none", width: "40px", clicker: "pointer"}}>
+                    <option className="text-info bg-dark" value ="/" >👤</option>
+                    <option className="text-info bg-dark" value ="Register">Register</option>
+                    <option className="text-info bg-dark" value ="Login">Login</option>
+                    <option className="text-info bg-dark" value ="/Profile">Support</option>
+                  </select>
                   </Link>
                 </div>
               </li>
