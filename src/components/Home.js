eda25a58fe526c274ab3faf35c859a69f80a689e
Simu <kangtaeam@gmail.com>
2022-08-18 00:09:14 -0400

    added announcement section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,12 @@ import React from 'react'
 const Home = () => {
     return (
        <> <header
-            className="py-5"
-            style={{
-              backgroundImage:
-                "url(https://pbs.twimg.com/media/FOZKlglUcAIoB2u?format=jpg&name=large)",
-            }}>
+            className="py-5" style={{ backgroundImage: "url(https://pbs.twimg.com/media/FOZKlglUcAIoB2u?format=jpg&name=large)",}}>
             <div className="container px-5">
               <div className="row gx-5 justify-content-center">
                 <div className="col-lg-6">
                   <div className="text-center my-5">
-                    <h1 className="display-5 fw-bolder text-white mb-2">
+                    <h1 className="display-5 fw-bolder text-info mb-2">
                       The Latest and Greatest Gaming Gear
                     </h1>
                     <p className="lead text-white mb-4">
@@ -21,14 +17,22 @@ const Home = () => {
                     <div className="d-grid gap-3 d-sm-flex justify-content-sm-center">
                       <a className="btn btn-primary btn-lg px-4 me-sm-3"
                         href="/store">Browse</a>
-                      <a className="btn btn-outline-light btn-lg px-4"
-                        href="/">Learn More</a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </header>
+          <section className="bg-light pt-5 pb-5" style={{backgroundImage: "url(https://wallpaper.dog/large/5538330.jpg)"}} id="services">
+            <div className="container px-4">
+                <div className="row gx-4 justify-content-center">
+                    <div className="col-lg-8 text-success mb-2">
+                        <h2 className="fw-bolder">Change the font ANNOUNCING CALL OF DUTY®:</h2>
+                        <h4>THE FRANCHISE SHOWCASE EVENT, AND OPEN BETA DATE TIMES FOR CALL OF DUTY®: MODERN WARFARE® II. Mark your calendars for September 15: The future of Call of Duty® will be revealed at Call of Duty: Next, a Franchise Showcase Event. Then clear your schedule for September 16–-26: Detailed Beta dates and timing is locked and loaded. </h4>
+                    </div>
+                </div>
+            </div>
+        </section>
           <section className="py-5 border-bottom" id="features">
             <div className="container px-5 my-5">
               <div className="row gx-5">
@@ -60,11 +64,12 @@ const Home = () => {
               </div>
             </div>
           </section>
+
           <section className="py-5 border-bottom">
             <div className="container px-5 my-5 px-5">
               <div className="text-center mb-5">
-                <h2 className="fw-bolder">Gamer testimonials</h2>
-                <p className="lead mb-0">Our gamers from all over the world love using our app!</p>
+                <h2 className="fw-bolder">Gamer Reviews</h2>
+                <p className="lead mb-0">Our gamers from all over the world love using our website!</p>
               </div>
               <div className="row gx-5 justify-content-center">
                 <div className="col-lg-6">
