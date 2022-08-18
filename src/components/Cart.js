ccb901aba888d5c95b6a77f09776cc88cd7e6693
PrestonS95 <prestonsullins95@gmail.com>
2022-08-18 08:57:54 -0500

    trying to map

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,56 +1,124 @@
-import React, { useState, useEffect, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import {  useNavigate  } from 'react-router-dom'
-import { getUserProfile, getUserCarts } from "../databaseAdapter";
 import { getUserProfile, getUserCarts } from "../databaseAdapter";
 
 
 
 
 const Cart = ({ userInfo, setUserInfo }) => {
-    const [userCart, setUserCart] = useState({})
+    const [userCart, setUserCart] = useState([])
+    let token = localStorage.getItem("token");
+    console.log(userInfo, token)
+     const getMyInfo = async () => {
+       const response = await getUserProfile(token);
+       console.log(token);
+        console.log(response, "Message Please Read");
+        setUserInfo(response);
+        const datad = await getUserCarts(token, userInfo.id)
+         setUserCart(datad)
+       console.log(datad);
+     };;
+     useEffect(() => {
+       getMyInfo();
+     }, []);
 
-    useEffect(() => {
-        let token = localStorage.getItem("token");
-        console.log(token);
-        async function getUserInfo() {
-            try {
-                const response = await getUserProfile(token);
-                console.log(token);
-                console.log(response, "Message Please Read");
-                setUserInfo(response);
-                const data = await getUserCarts(token, userInfo.id)
-                console.log(data, 'getCartById data')
-                setUserCart(data)
+    //  const handleChange = () => {
+       
+    //  };
 
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getUserInfo();
-    }, []);
+    //  const handleSubmit = async (event) => {
+    //    event.preventDefault();
+       
+    //   ;
+    //  };
+
+
+    // useEffect(() => {
+    //     let token = localStorage.getItem("token");
+    //     console.log(token);
+    //     async function getUserInfo() {
+    //         try {
+    //             const response = await getUserProfile(token);
+    //             console.log(token);
+    //             console.log(response, "Message Please Read");
+    //             setUserInfo(response);
+    //             const data = await getUserCarts(token, userInfo.id)
+    //             console.log(data, 'getCartById data')
+    //             setUserCart(data)
+
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     }
+    //     getUserInfo();
+    // }, []);
     console.log(userInfo, 'userinfo')
-    useEffect(() => {
-        let token = localStorage.getItem("token");
-        console.log(token);
-        async function getUserCartInfo() {
-            try {
-                const data = await getUserCarts(token, userInfo.id);
-                setUserCart(data);
-                console.log(data, "getCartById data");
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getUserCartInfo();
-    }, []);
+    // useEffect(() => {
+    //     let token = localStorage.getItem("token");
+    //     console.log(token);
+    //     async function getUserCartInfo() {
+    //         try {
+    //             const data = await getUserCarts(token, userInfo.id);
+    //             setUserCart(data);
+    //             console.log(data, "getCartById data");
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     }
+    //     getUserCartInfo();
+    // }, []);
 
-    let Navigate = useNavigate()
-    const handleClick = () => {
+    // let Navigate = useNavigate()
+    // const handleClick = () => {
 
-    }
+    // }
 
     return (
-      <>
+
+         <div>
+      <h1 className="text-center">Shop</h1>
+      <div className="storeContainer">
+        {userCart.length
+          ? userCart.products.map((products, index) => {
+            console.log(products)
+            return (
+              <div key={index} className="mx-auto my-5">
+
+                <div className="card productsCard">
+                  <div className="card-body d-flex flex-row">
+                    <div>
+                      <h5 className="card-title font-weight-bold mb-2 text-center" style={{ height: "50px" }}>{products.title}</h5>
+                      <div className="priceCartBar">
+                        <div className="card-text">{products.price}</div>
+                        <button>Add to Cart</button>
+                      </div>
+
+                        <div className="carousel-inner">
+                          <div className="carousel-item active">
+                            <img src={products.image_1}
+                              className="d-block w-100 object-cover object-center" height="300px" alt="img1" />
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        {products.description}
+                      </div>
+
+                    </div>
+                  </div>
+
+
+
+
+                </div>
+
+            );
+          })
+          : 
+  
+
+
+      (<>
         <h1>Cart gang</h1>
         <section className='h-100 h-custom' styles='background-color: #d2c9ff;'>
           <div className='container py-5 h-100'>
@@ -67,16 +135,18 @@ const Cart = ({ userInfo, setUserInfo }) => {
                             <h1 className='fw-bold mb-0 text-black'>
                               Shopping Cart
                             </h1>
-                             <h6 className='mb-0 text-muted'>`3 items</h6>
-                          </div>
-                          {userCart.map((cart, idx) => {
-                            return (
-                                <div className='cartProducts'>
-                                  <hr className='my-4'></hr>
+                            
+                          { userCart.products.map( (cart, idx) => {
+                            
 
-                                  <div
-                                    key={idx}
-                                    className='row mb-4 d-flex justify-content-between align-items-center'>
+                            
+                            console.log(cart, idx, 'cart idx')
+                            return (
+                                <div
+                                  className='cartProducts'
+                                  key={cart.id}>
+                                  <hr className='my-4'></hr>
+                                  <div className='row mb-4 d-flex justify-content-between align-items-center'>
                                     <div className='col-md-2 col-lg-2 col-xl-2'>
                                       <img
                                         src={cart.image_1}
@@ -88,14 +158,12 @@ const Cart = ({ userInfo, setUserInfo }) => {
                                         {cart.brand}
                                       </h6>
                                       <h6 className='text-black mb-0'>
-                                        {cart.title}
+                                        <cart.title/>
                                       </h6>
                                     </div>
                                     <div className='col-md-3 col-lg-3 col-xl-2 d-flex'>
                                       <div styles='width: 50px;'>
-                                        <h5 className='fw-normal mb-0'>
-                                          Quantity:{cart.quantity}
-                                        </h5>
+                                        <h5 className='fw-normal mb-0'>`Quantity: ${cart.quantity}`</h5>
                                       </div>
                                     </div>
                                     <div className='col-md-3 col-lg-2 col-xl-2 offset-lg-1'>
@@ -110,10 +178,9 @@ const Cart = ({ userInfo, setUserInfo }) => {
                                     </div>
                                   </div>
                                 </div>
-                              
-                            );
-                          })}{" "}
-                          : (<>null</>)
+                          )  ;
+                          })}
+                         
                           <div className='pt-5'>
                             <h6 className='mb-0'>
                               <a href='#!' className='text-body'>
@@ -127,11 +194,11 @@ const Cart = ({ userInfo, setUserInfo }) => {
                         <div className='col-lg-4 bg-grey'>
                           <div className='card bg-primary text-white rounded-3'>
                             <div className='card-body'>
-                              <div className='d-flex justify-content-between align-items-center mb-4'>
+                              {<div className='d-flex justify-content-between align-items-center mb-4'>
                                 <h5 className='mb-0'>Card details</h5>
-                                { <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-                        className="img-fluid rounded-3" styles="width: 45px;" alt="Avatar"></img> }
-                              </div>
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+                                    className="img-fluid rounded-3" styles="width: 45px;" alt="Avatar"></img> 
+                              </div>}
 
                               <p className='small mb-2'>Card type</p>
                               <a href='#!' type='submit' className='text-white'>
@@ -148,67 +215,67 @@ const Cart = ({ userInfo, setUserInfo }) => {
                               </a>
 
                               <form className='mt-4'>
-                                <div className='form-outline form-white mb-4'>
+                                <div className='htmlForm-outline htmlForm-white mb-4'>
                                   <input
                                     type='text'
                                     id='typeName'
-                                    className='form-control form-control-lg'
+                                    className='htmlForm-control htmlForm-control-lg'
                                     size='17'
                                     placeholder="Cardholder's Name"
                                   />
-                                  <label className='form-label' for='typeName'>
+                                  <label className='htmlForm-label' htmlFor='typeName'>
                                     Cardholder's Name
                                   </label>
                                 </div>
 
-                                <div className='form-outline form-white mb-4'>
+                                <div className='htmlForm-outline htmlForm-white mb-4'>
                                   <input
                                     type='text'
                                     id='typeText'
-                                    className='form-control form-control-lg'
+                                    className='htmlForm-control htmlForm-control-lg'
                                     size='17'
                                     placeholder='1234 5678 9012 3457'
                                     minLength='19'
                                     maxLength='19'
                                   />
-                                  <label className='form-label' for='typeText'>
+                                  <label className='htmlForm-label' htmlFor='typeText'>
                                     Card Number
                                   </label>
                                 </div>
 
                                 <div className='row mb-4'>
                                   <div className='col-md-6'>
-                                    <div className='form-outline form-white'>
+                                    <div className='htmlForm-outline htmlForm-white'>
                                       <input
                                         type='text'
                                         id='typeExp'
-                                        className='form-control form-control-lg'
+                                        className='htmlForm-control htmlForm-control-lg'
                                         placeholder='MM/YYYY'
                                         size='7'
                                         minLength='7'
                                         maxLength='7'
                                       />
                                       <label
-                                        className='form-label'
-                                        for='typeExp'>
+                                        className='htmlForm-label'
+                                        htmlFor='typeExp'>
                                         Expiration
                                       </label>
                                     </div>
                                   </div>
                                   <div className='col-md-6'>
-                                    <div className='form-outline form-white'>
+                                    <div className='htmlForm-outline htmlForm-white'>
                                       <input
                                         type='password'
                                         id='typeText'
-                                        className='form-control form-control-lg'
+                                        className='htmlForm-control htmlForm-control-lg'
                                         placeholder='&#9679;&#9679;&#9679;'
                                         size='1'
                                         minLength='3'
                                         maxLength='3'
                                       />
                                       <label
-                                        className='form-label'
-                                        for='typeText'>
+                                        className='htmlForm-label'
+                                        htmlFor='typeText'>
                                         Cvv
                                       </label>
                                     </div>
@@ -218,10 +285,10 @@ const Cart = ({ userInfo, setUserInfo }) => {
 
                               <hr className='my-4'></hr>
 
-                              <div className='d-flex justify-content-between'>
-                                <p className='mb-2'>Subtotal</p>
-                                <p className="mb-2">$4798.00{userCart.products.forEach((e)=>{return e.quantity + e.price })}</p>
-                              </div>
+                              {/* <div className='d-flex justify-content-between'>
+                                <p className='mb-2'>Subtotal</p> */}
+                                {/* <p className="mb-2">$4798.00{userCart.products.htmlForEach((e)=>{return e.quantity + e.price })}</p>
+                              </div> */}
 
                               <div className='d-flex justify-content-between'>
                                 <p className='mb-2'>Shipping</p>
@@ -254,9 +321,11 @@ const Cart = ({ userInfo, setUserInfo }) => {
               </div>
             </div>
           </div>
+          </div>
         </section>
       </>
-    );
-}
+      )}</div>
+    </div>
+)}
 
 export default Cart;
