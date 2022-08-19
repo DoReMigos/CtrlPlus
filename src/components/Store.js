ce31115286a730422afd5af683ef05e12920bd46
PrestonS95 <prestonsullins95@gmail.com>
2022-08-18 21:31:53 -0500

    merge fixes

--- src/components/Store.js
+++ src/components/Store.js
@@ -139,15 +139,15 @@ useEffect(()=>{
                         </button>
                       </div>
 
-                      <div class="accordion accordion-flush" id="accordionFlushExample">
-                        <div class="accordion-item">
-                          <h2 class="accordion-header" id="flush-headingOne">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                      <div className="accordion accordion-flush" id="accordionFlushExample">
+                        <div className="accordion-item">
+                          <h2 className="accordion-header" id="flush-headingOne">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                               Description
                             </button>
                           </h2>
-                          <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">{products.description}</div>
+                          <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                            <div className="accordion-body">{products.description}</div>
                           </div>
                         </div>
                       </div>
@@ -157,7 +157,7 @@ useEffect(()=>{
 
                           showEdit != products.id ?
 
-                                <button onClick={() => handleEditSelect(products.id)}class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseOne">
+                                <button onClick={() => handleEditSelect(products.id)}className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseOne">
                                   Edit or Delete
                                 </button>
 :
