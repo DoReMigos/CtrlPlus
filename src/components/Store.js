9054d6cbd60bc8e55a331c3f177a6caab0872f6a
Jessica <kimjessicad@gmail.com>
2022-08-22 16:01:31 -0500

    description, carousel-setup, pagination setup

--- src/components/Store.js
+++ src/components/Store.js
@@ -3,6 +3,7 @@ import { getAllProducts, getUserProfile, deleteProduct, getUserCarts } from "../
 import AdminUpdate from "./AdminUpdate";
 import AdminCreate from "./AdminCreate";
 import AddToCart from "./AddToCart"
+import ImageCarousel from "./ImageCarousel"
 import "./Store.css"
 import { addProductToCart } from "../databaseAdapter";
 import LoadingScreen from "./Loading"
@@ -12,14 +13,16 @@ import LoadingScreen from "./Loading"
 export default function Store({ userInfo, setUserInfo }) {
   const [allProducts, setAllProducts] = useState([]);
   const [showEdit, setShowEdit] = useState(null)
+  const [showDescription, setShowDescription] = useState(null)
   const [selectedPage, setSelectedPage] = useState(1)
-  const [volumeSelect, setVolumeSelect] = useState(20)
+  const [volumeSelect, setVolumeSelect] = useState(40)
   const [productsToShow, setProductsToShow] = useState([])
   const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     async function fetchProducts() {
       const returnProducts = await getAllProducts();
-      
+
       setAllProducts(returnProducts)
       console.log(returnProducts)
         ;
@@ -27,8 +30,6 @@ export default function Store({ userInfo, setUserInfo }) {
     setTimeout(() => setLoading(false), 2000)
     fetchProducts();
   }, [])
-
-
 
   useEffect(() => {
     let token = localStorage.getItem("token");
@@ -53,12 +54,35 @@ export default function Store({ userInfo, setUserInfo }) {
     window.location.reload(true);
   }
 
+  async function handleClick1(event) {
+    event.preventDefault()
+    setSelectedPage(1);
+  }
+
+  async function handleClick2(event) {
+    event.preventDefault()
+    setSelectedPage(2);
+  }
+
+  async function handleClick3(event) {
+    event.preventDefault()
+  }
+
+  async function handleClick4(event) {
+    event.preventDefault()
+  }
+  
+
   const isAdmin = userInfo.isAdmin
   console.log(userInfo, "this is userInfo on Store")
   console.log(isAdmin, "this is isAdmin on Store Page")
   function handleEditSelect(productId) {
     setShowEdit(productId)
   }
+  function handleDescriptionSelect(productId) {
+    setShowDescription(productId)
+  }
+
   useEffect(() => {
     if (allProducts.length) {
       const listOfProducts = allProducts.filter((_, index) => {
@@ -73,134 +97,100 @@ export default function Store({ userInfo, setUserInfo }) {
     }
   }, [allProducts])
 
+
+
   return (
     <>
-    {loading === false ? 
-    <div>
-      <h1 className="text-center">Store</h1>
-      {isAdmin ? (<AdminCreate allProducts={allProducts} setAllProducts={setAllProducts} />) : null}
-      <div className="storeContainer">
-        {productsToShow.length
-          ? productsToShow.map((products, index) => {
-            const productId = products.id
-            return (
-              <div key={`${products.id}`} className="mx-auto my-5">
+      {loading === false ?
+        <div className="bg-dark">
+          <h1 className="text-center" style={{ color: "white" }}>Store</h1>
+          {isAdmin ? (<AdminCreate allProducts={allProducts} setAllProducts={setAllProducts} />) : null}
+          <div className="storeContainer bg-dark">
+            {productsToShow.length
+              ? productsToShow.map((products, index) => {
+                const productId = products.id
+                const slides = [
+                  products.image_1,
+                  products.image_2,
+                  products.image_3,
+                  products.image_4
+                ]
+                return (
+                  <div key={`${products.id}`} className="mx-auto my-5">
 
-                <div className="card productsCard">
-                  <div className="card-body d-flex flex-row">
-                    <div>
-                      <h5 className="card-title font-weight-bold mb-2 text-center" style={{ height: "50px" }}>{products.title}</h5>
-                      <div className="priceCartBar">
-                        <div className="card-text">{products.price}</div>
-                        <AddToCart products={products} userInfo={userInfo} />
-                      </div>
+                    <div className="card productsCard" style={{ background: "gray" }}>
+                      <div className="card-body d-flex flex-row">
+                        <div>
+                          <h5 className="card-title font-weight-bold mb-2 text-center" style={{ height: "50px" }}>{products.title}</h5>
+                          <div className="priceCartBar">
+                            <div className="card-text">{products.price}</div>
+                            <AddToCart products={products} userInfo={userInfo} />
+                          </div>
 
-                      <div id="carouselExampleIndicators" className="carousel slide" data-mdb-ride="carousel">
-                        <div className="carousel-indicators">
-                          <button
-                            type="button"
-                            data-mdb-target="#carouselExampleIndicators"
-                            data-mdb-slide-to="0"
-                            className="active"
-                            aria-current="true"
-                            aria-label="Slide 1"
-                          ></button>
-                          <button
-                            type="button"
-                            data-mdb-target="#carouselExampleIndicators"
-                            data-mdb-slide-to="1"
-                            aria-label="Slide 2"
-                          ></button>
-                          <button
-                            type="button"
-                            data-mdb-target="#carouselExampleIndicators"
-                            data-mdb-slide-to="2"
-                            aria-label="Slide 3"
-                          ></button>
-                          <button
-                            type="button"
-                            data-mdb-target="#carouselExampleIndicators"
-                            data-mdb-slide-to="3"
-                            aria-label="Slide 4"
-                          ></button>
-                        </div>
-                        <div className="carousel-inner">
-                          <div className="carousel-item active">
-                            <img src={products.image_1}
-                              className="d-block w-100 object-cover object-center" height="300px" alt="img1" />
-                          </div>
-                          <div className="carousel-item">
-                            <img src={products.image_2}
-                              className="d-block w-100 object-cover object-center" alt="img2" />
-                          </div>
-                          <div className="carousel-item">
-                            <img src={products.image_3}
-                              className="d-block w-100 object-cover object-center" alt="img3" />
-                          </div>
-                          <div className="carousel-item">
-                            <img src={products.image_4}
-                              className="d-block w-100 object-cover object-center" alt="img4" />
-                          </div>
-                        </div>
-                        <button className="carousel-control-prev" type="button" data-mdb-target="#carouselExampleIndicators" data-mdb-slide="prev">
-                          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-mdb-target="#carouselExampleIndicators" data-mdb-slide="next">
-                          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span className="visually-hidden">Next</span>
-                        </button>
-                      </div>
+                          <ImageCarousel products={products} />
 
-                      <div className="accordion accordion-flush" id="accordionFlushExample">
-                        <div className="accordion-item">
-                          <h2 className="accordion-header" id="flush-headingOne">
-                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                              Description
-                            </button>
-                          </h2>
-                          <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                            <div className="accordion-body">{products.description}</div>
+                          <div style={{ marginTop: "30px" }}>
+                            {showDescription != products.id ?
+                              <button onClick={() => handleDescriptionSelect(products.id)} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseOne">
+                                Description
+                              </button>
+                              :
+                              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <div>{products.description}</div>
+                                <button onClick={() => { setShowDescription(null) }} className="btn btn-dark">Hide Description</button>
+                              </div>}
+                          </div>
+
+
+
+                          <div>
+                            {isAdmin ? (
+
+                              showEdit != products.id ?
+                                <button onClick={() => handleEditSelect(products.id)} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseOne">
+                                  Edit or Delete
+                                </button>
+                                :
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                  <button onClick={() => { handleDelete(productId) }} className="btn btn-dark" style={{ marginBottom: "10px" }}>Delete product</button>
+                                  <AdminUpdate products={products} />
+                                  <button onClick={() => { setShowEdit(null) }} className="btn btn-dark">Hide Menu</button>
+                                </div>
+                            ) : null}
                           </div>
                         </div>
                       </div>
 
-                      <div>
-                        {isAdmin ? (
 
-                          showEdit != products.id ?
-                            <button onClick={() => handleEditSelect(products.id)} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseOne">
-                              Edit or Delete
-                            </button>
-                            :
-                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                              <button onClick={() => { handleDelete(productId) }} className="btn btn-dark" style={{ marginBottom: "10px" }}>Delete product</button>
-                              <AdminUpdate products={products} />
-                              <button onClick={() => { setShowEdit(null) }} className="btn btn-dark">Hide Menu</button>
-                            </div>
-                        ) : null}
-                      </div>
+
+
                     </div>
+
                   </div>
+                );
+              })
+              : null}
+          </div>
 
-
-
-
-                </div>
-
+          <div className="pagination">
+            {/* {selectedPage === 1 ? ( */}
+              <div>
+                <button onClick={ handleClick1 }>1</button>
+                <button onClick={ handleClick2 }>2</button>
+                <button onClick={ handleClick3 }>3</button>
+                <button onClick={ handleClick4 }>4</button>
               </div>
-            );
-          })
-          : null}
-      </div>
-      {/* create buttons for page numbers/previous/next
+            {/* ) : null
+            } */}
+          </div>
+          {/* create buttons for page numbers/previous/next
             onClick for specific numbers goes into a handleClick function that will set selected page as the template literal for the selected page
             previous and next buttons will +-1 for selected page, but need edge cases if you're on the first page or last page that it disables the prev or next button -- this is probably extra but looks nice. can technically do this just with the numbers and no prev/next if it's too time consuming/difficult.
          */}
 
 
-    </div>
-: <LoadingScreen />}
+        </div>
+        : <LoadingScreen />}
     </>
   )
 }
