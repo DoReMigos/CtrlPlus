d98581e3552a514c44cd201077373946056f45b2
Jessica <kimjessicad@gmail.com>
2022-08-22 16:03:44 -0500

    description, carousel setup, pagination setup

--- src/components/Carousel.js
+++
@@ -1,8 +0,0 @@
-// const track = document.querySelector('.carouselTrack');
-// const slides = Array.from(track.children);
-// const nextButton = document.querySelector('.carButton--Right');
-// const prevButton = document.querySelector('.carButton--Left');
-// const dotsNavs = document.querySelector('.carouselNav');
-// const dots = Array.from(dotsNav.children);
-
-// slides.forEach((slide,index) =>)
