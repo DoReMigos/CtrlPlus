141615680068a3606cab28acd20b6ce186f531b7
Jessica <kimjessicad@gmail.com>
2022-08-23 20:26:51 -0500

    merging

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
 import "./ImageSlider.css"
@@ -42,28 +41,3 @@ export default function ImageSlider({ products }) {
         </div>
     )
 }
-=======
-// import React, {useState} from "react";
-// import {BsChevronDoubleLeft, BsChevronDoubleRight} from 'react-icons'
-
-// export default function ImageSlider({products}){
-//     const [current, setCurrent] = useState(0)
-//     const length = slides.length
-
-//     const sliderData = [
-//         {image: products.image_1},
-//         {image: products.image_2},
-//         {image: products.image_3},
-//         {image: products.image_4}
-//     ]
-
-//     return(
-//         <div className="slider">
-
-//             {sliderData.map((slide,index) => {
-//                 return <img src={slide.image} alt='item image'/>
-//             })}
-//         </div>
-//     )
-// }
->>>>>>> origin/homer
